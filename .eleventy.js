f73aaff1e80310132f42164396f8d0320be4c969
Matthew Perry <mattp@tuta.io>
2020-12-24 13:48:14 -0500

    created article list

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy("et-book");
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    });
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    });
     return {
         passthroughFileCopy: true,
         dir: { output: "dist" },
---
+++ articles/index.md
@@ -0,0 +1,8 @@
+---
+Title: Articles
+layout: base.njk
+date: Last Modified
+---
+## My Articles
+
+{% include "articlelist.njk" %}
