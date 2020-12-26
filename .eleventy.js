355d6e4b122174d8cad0a21ad840963a8f52dc57
Matthew Perry <mattp@tuta.io>
2020-12-26 13:56:16 -0500

    added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css');
-    eleventyConfig.addPassthroughCopy("et-book");
+    eleventyConfig.addPassthroughCopy('et-book');
+    eleventyConfig.addPassthroughCopy('images')
     eleventyConfig.addFilter("readableDate", dateObj => {
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
     });
