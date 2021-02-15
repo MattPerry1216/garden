c0fd0a929e3e15d0d87a67c8cfe357c67adc1ece
Matthew Perry <mattp@tuta.io>
2021-02-15 10:11:17 -0500

    Add robots.txt and sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
 const { DateTime } = require("luxon");
+const format = require("date-fns/format")
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('et-book');
     eleventyConfig.addPassthroughCopy('images')
+
+    // Creates human readable formatted date for articles
     eleventyConfig.addFilter("readableDate", dateObj => {
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
     });
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
         return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    });
+
+    // Creates date filter for sitemap
+    eleventyConfig.addFilter('date', function (date, dateFormat) {
+        return format(date, dateFormat)
     });
     return {
         passthroughFileCopy: true,
